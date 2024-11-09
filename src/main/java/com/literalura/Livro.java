3a3ae85e65e42636219f632deb04171247ea762e
Rafael <rafael.felix.moraes@gmail.com>
2024-11-09 12:57:31 -0300

    feat: criando classe Livro

---
+++ src/main/java/com/literalura/Livro.java
@@ -0,0 +1,22 @@
+package com.literalura;
+
+import com.google.gson.annotations.SerializedName;
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "livros")
+public class Livro {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String title;
+
+    private String authors;
+
+    private String languagens;
+
+    @SerializedName("download_count")
+    private Long downloadCount;
+}
